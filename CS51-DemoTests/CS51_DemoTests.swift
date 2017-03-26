8216fd06a2fbbe99749ef659ba68ad53eb6f0c89
Nathan Eidelson <nathan.eidelson@gmail.com>
2017-03-26 16:23:45 -0700

    Initial Commit

---
+++ CS51-DemoTests/CS51_DemoTests.swift
@@ -0,0 +1,36 @@
+//
+//  CS51_DemoTests.swift
+//  CS51-DemoTests
+//
+//  Created by Nathan Eidelson on 3/26/17.
+//  Copyright © 2017 Nathan Eidelson. All rights reserved.
+//
+
+import XCTest
+@testable import CS51_Demo
+
+class CS51_DemoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
